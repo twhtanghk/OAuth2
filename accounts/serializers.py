52a91b908da909586e4295bbcff4e16afe10d7a1
root <twhtanghk@gmail.com>
2016-12-20 18:16:40 +0800

    remove no longer used default getter method

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    url = serializers.SerializerMethodField('get_url')
+    url = serializers.SerializerMethodField()
     
     class Meta:
         model = User
