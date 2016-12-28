5050a882d29706af1a714288ef7031ac29d3b689
root <twhtanghk@gmail.com>
2016-12-28 09:29:44 +0800

    upgrade rest_framework

--- accounts/views.py
+++ accounts/views.py
@@ -21,55 +21,35 @@ from registration.models import RegistrationProfile
 from django.core.mail import EmailMultiAlternatives
 from warnings import warn
 from rest_framework.generics import RetrieveAPIView
-from rest_framework import filters, permissions
+from rest_framework import filters, permissions, decorators
+from oauth2_provider.ext.rest_framework import OAuth2Authentication
+from registration.backends.hmac import views
+from django.contrib.sites.shortcuts import get_current_site
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    User view for list/create/retrieve/readMe/update/delete of user account in JSON format only
-    
-    list:    for authenticated user
-    create:  deny to create user via api, use web interface only
-    retrieve:for authenticated user
-    readMe:  for authenticated user
-    update:  for authenticated user and owner only
-    delete:  deny to delete user via api, use web interface only
-    """
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     model = User
     queryset = User.objects.all()
     renderer_classes = [JSONRenderer]
     serializer_class = UserSerializer
     lookup_field = 'username'
+    permission_classes = (permissions.AllowAny,)
     lookup_value_regex = '[^/]*'
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.SearchFilter, filters.OrderingFilter,)
     search_fields = ('username', 'email')
     ordering_fields = ('username', 'email')
     ordering = ('username',)
     
-    def create(self, request, *args, **kwargs):
-        raise PermissionDenied
-    
-    def readMe(self, request, *args, **kwargs):
-        kwargs[self.lookup_field] = request.user.username
-        self.kwargs[self.lookup_field] = request.user.username
-        return super(UserViewSet, self).retrieve(request, *args, **kwargs)
-    
-    def update(self, request, *args, **kwargs):
-        if kwargs[self.lookup_field] != request.user.username:
-            raise PermissionDenied
-        return super(UserViewSet, self).update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        if kwargs[self.lookup_field] != request.user.username:
-            raise PermissionDenied
-        return super(UserViewSet, self).delete(request, *args, **kwargs)
-    
-class UserExists(RetrieveAPIView):
+class UserMe(RetrieveAPIView):
     model = User
     renderer_classes = [JSONRenderer]
-    serializer_class = UserExistsSerializer
+    serializer_classes = UserSerializer
     lookup_field = 'username'
-    permission_classes = ()
+    authentication_classes = (OAuth2Authentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def retrieve(self, request, *args, **kwargs):
+        kwargs[self.lookup_field] = request.user.username
+        self.kwargs[self.lookup_field] = request.user.username
     
 class UserRead(LoginRequiredMixin, DetailView):
     """
@@ -108,91 +88,16 @@ class UserDelete(LoginRequiredMixin, ModelFormMixin, DeleteView):
             raise PermissionDenied
         return super(UserDelete, self).dispatch(request, *args, **kwargs)
     
-class RegistrationView(BaseRegistrationView):
-    """
-    Revised RegistrationView of django-registration to support html email
-    
-    A registration backend which follows a simple workflow:
-
-    1. User signs up, inactive account is created.
-
-    2. Email is sent to user with activation link.
-
-    3. User clicks activation link, account is now active.
-
-    Using this backend requires that
-
-    * ``registration`` be listed in the ``INSTALLED_APPS`` setting
-      (since this backend makes use of models defined in this
-      application).
-
-    * The setting ``ACCOUNT_ACTIVATION_DAYS`` be supplied, specifying
-      (as an integer) the number of days from registration during
-      which a user may activate their account (after that period
-      expires, activation will be disallowed).
-
-    * The creation of the templates
-      ``registration/activation_email_subject.txt`` and
-      ``registration/activation_email.txt``, which will be used for
-      the activation email. See the notes for this backends
-      ``register`` method for details regarding these templates.
-
-    Additionally, registration can be temporarily closed by adding the
-    setting ``REGISTRATION_OPEN`` and setting it to
-    ``False``. Omitting this setting, or setting it to ``True``, will
-    be interpreted as meaning that registration is currently open and
-    permitted.
-
-    Internally, this is accomplished via storing an activation key in
-    an instance of ``registration.models.RegistrationProfile``. See
-    that model and its custom manager for full documentation of its
-    fields and supported operations.
-    
-    """
-    def register(self, request, **cleaned_data):
-        """
-        Given a username, email address and password, register a new
-        user account, which will initially be inactive.
-
-        Along with the new ``User`` object, a new
-        ``registration.models.RegistrationProfile`` will be created,
-        tied to that ``User``, containing the activation key which
-        will be used for this account.
-
-        An email will be sent to the supplied email address; this
-        email should contain an activation link. The email will be
-        rendered using two templates. See the documentation for
-        ``RegistrationProfile.send_activation_email()`` for
-        information about these templates and the contexts provided to
-        them.
-
-        After the ``User`` and ``RegistrationProfile`` are created and
-        the activation email is sent, the signal
-        ``registration.signals.user_registered`` will be sent, with
-        the new ``User`` as the keyword argument ``user`` and the
-        class of this backend as the sender.
-
-        """
-        username, email, password = cleaned_data['username'], cleaned_data['email'], cleaned_data['password1']
-        if Site._meta.installed:
-            site = Site.objects.get_current()
-        else:
-            site = RequestSite(request)
-        new_user = RegistrationProfile.objects.create_inactive_user(username.lower(), email,
-                                                                    password, site,
-                                                                    False)
-        profile = new_user.registrationprofile_set.all()[0]
-
-        self.send_activation_email(site, profile)
-        signals.user_registered.send(sender=self.__class__,
-                                     user=new_user,
-                                     request=request)
+class RegistrationView(views.RegistrationView):
+    def register(self, form):
+        new_user = self.create_inactive_user(form)
+        self.send_activation_email(new_user)
         return new_user
     
-    def send_activation_email(self, site, profile):
-        user = profile.user
+    def send_activation_email(self, user):
+        site = get_current_site(self.request)
         ctx_dict = {
-            'activation_key': profile.activation_key,
+            'activation_key': self.get_activation_key(user),
             'expiration_days': settings.ACCOUNT_ACTIVATION_DAYS,
             'site': site,
             'email': user.email,
